1ca585224dffd96de97ebe25cc28150006d6460a
金灿 <xyjincan@qq.com>
2020-04-22 09:16:35 +0800

    Create weibologin.js

---
+++ weibologin.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Web home
+// @namespace    https://www.weibo.com/
+// @version      0.1
+// @description  web home
+// @author       xyjinca
+// @match        https://www.weibo.com/nguide/interests
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location.replace("https://www.weibo.com/");
+})();
